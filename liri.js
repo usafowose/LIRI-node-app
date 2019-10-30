123f5d0e9825e522d5cbd150b31dd7152d0488a0
Andrew Fowose <usafowose@gmail.com>
2019-10-30 13:48:31 -0400

    Functioning Spotify, IMBD, and default functions and calls

--- liri.js
+++ liri.js
@@ -1,44 +1,106 @@
+// DEPENDENCIES
+
 require("dotenv").config();
 var keys = require("./keys.js");
 var axios = require("axios");
+var Spotify = require('node-spotify-api');
+var moment = require('moment');
+var fs = require("fs");
+
+// CLI VARIABLES
 
 var command = process.argv[2];
 var subject = process.argv.slice(3).join('+');
+
+
+// FIND CONCERT
 
 var findConcert = function () {
     var URL = "https://rest.bandsintown.com/artists/" + subject + "/events?app_id=codingbootcamp";
     axios.get(URL).then(function (response) {
         var concertData = response.data[0]
-        var concertVenue = concertData.venue.name; 
+        var concertVenue = concertData.venue.name;
         var venueCity = concertData.venue.city;
         var venueState = concertData.venue.region;
-        var concertDate = concertData.datetime;
-        var n = ('\n'); 
+        var concertDate = moment(concertData.datetime).format('MM-DD-YYYY');
+        var n = ('\n');
         console.log(`Venue Name: ${concertVenue} ${n}Venue Location: ${venueCity}, ${venueState} ${n}Concert Date: ${concertDate}`);
     });
 }
 
 
-var Spotify = require('node-spotify-api');
- 
-var spotify = new Spotify(
-    {
-  id: keys.spotify.id,
-  secret: keys.spotify.secret,
-}
-);
+// SPOTIFY
+
+var spotify = new Spotify(keys.spotify)
 var findSong = function () {
-    var URL = "https://api.spotify.com/v1/search/type=track&q=" + subject + "&id=" + spotify["id"]; 
 
-axios.get(URL).then(function (response){
-    console.log(URL);
-    console.log(response);
-})
-.catch((error)=>{
-    console.log(error)
-  });
+    if (!subject) {
+        subject = "The Sign"
+        console.log("No song was passed in input, so here's a song you may be interested in.");
+    };
+
+    var URL = "https://api.spotify.com/v1/search/type=track&q=" + subject + "&id=" + spotify[0];
+    // CALL TO SPOTIFY API
+    spotify.search({ type: 'track', query: subject }, function (err, data) {
+        var songInfo =
+            `Artist Name: ${data.tracks.items[0].artists[0].name}
+         Song Name: ${data.tracks.items[0].name}
+         Album: ${data.tracks.items[0].album.name}
+         Preview Link: ${data.tracks.items[0].preview_url}`
+        console.log(songInfo);
+        if (err) {
+            console.log(err);
+        }
+
+    });
+
 }
 
+// MOVIE SEARCH
+
+var findMovie = function () {
+    if (!subject) {
+        var nL = "\n";
+        console.log(`If you haven't watched 'Mr.Nobody', then you should. ${nL}It's on Netflix!`);
+        subject = "Mr. Nobody"
+    };
+    var URL = `https://www.omdbapi.com/?apikey=trilogy&t=${subject}&plot=full`;
+    axios.get(URL).then(function (response) {
+        console.log(URL);
+        var data = response.data
+        var movieData =
+        {
+            "Title:": data.Title,
+            "Year Released:": data.Year,
+            "IMDB Rating:": data.imdbRating,
+            "Rotten Tomatoes Rating:": data.Ratings[1].Value,
+            "Produced In:": data.Country,
+            "Language:": data.Language,
+            "Plot/Summary:": data.Plot,
+            "Actors": data.Actors,
+        };
+        console.log(movieData);
+    });
+};
+
+// DEFAULT FUNCTION
+var defaultFunction = function () {
+
+    fs.readFile("random.txt", "utf8", function (err, data) {
+
+        if (err) {
+            return console.log(err);
+        }
+        // console.log(data);
+        var dataArr = data.split(",");
+        console.log(dataArr);
+        command = dataArr[0];
+        subject = dataArr[1];
+        console.log(command, subject);
+        findSong();
+    });
+
+}
 
 if (command === "concert-this") {
     console.log(command, subject);
@@ -54,5 +116,4 @@ if (command === "concert-this") {
                 defaultFunction();
             };
 // Functions defined below
-
 
